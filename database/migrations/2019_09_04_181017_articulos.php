682246c4ac6ef6e7df8171ecafa7eda2e729380e
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2019-09-04 15:43:47 -0500

    Migraciones Articulos,  Categoría SubCategorria
    Se constuyeron las tablas para los articulos, con sus respectivas categorias y sus subcategorias.

---
+++ database/migrations/2019_09_04_181017_articulos.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Articulos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nombre');
+            $table->string('direc');
+
+            $table->string('precioCompra');
+            $table->string('marca');
+           
+            $table->unsignedInteger('stock');
+            $table->string('fecha_cad');
+
+            $table->string('precio_u');
+
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
