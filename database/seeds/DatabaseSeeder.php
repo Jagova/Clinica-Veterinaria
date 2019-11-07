e896efa16c9c5d6e86e06a23f99401bfbe1a5f10
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2019-11-06 21:48:52 -0600

    Seeders Medicamentos con imagenes
    Se agregaron seeders para agregar algunos registros para los medicamentos.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         //$this->call(duenos_seeder::class);//
-        $this->call('ClinicasTableSeeder');
+         $this->call('ClinicasTableSeeder');
          $this->call('CategoriasSeeder');
          $this->call('SubCategoriaSeeder');
          $this->call('ArticulosSeeder');
--- database/seeds/MedicamentosSeeder.php
+++ database/seeds/MedicamentosSeeder.php
@@ -7,24 +7,53 @@ class MedicamentosSeeder extends Seeder
 {
 
     public function run()
-    {
-/*         $faker = Faker::create();
-        for ($i=0; $i < 10; $i++) {
-        	\DB::table('medicamentos')->insert(array(
-            'nombre' => $faker->randomElement(['Inyecccion','Desparasintante','Pastilla para moquillo','Vendaje']),
-           'presentacion'  => $faker->randomElement(['3','4','3']),
-           'compuesto'  => $faker->randomElement(['perro feliz','GUAGUA','PETS']),
-           'ml_mg'  => $faker->randomElement(['10','50','100', '1','15','1000']),
-           'precio'  => $faker->randomElement([20,10,50]),
-           'laboratorio'  => $faker->randomElement(['umbrella','imss','neo-umbrella','viral']),
-           'paq_unidad'  => $faker->randomElement(['10','11','5']),
-           'controlado'  => $faker->randomElement(['No','Si']),
-           'fecha_caducidad'  => $faker->randomElement(['2019-02-06','2019-08-08','2019-07-07']),
-           'stock'  => $faker->randomElement(['9','6','100', '40','1','10']),
-           'created_at' => date('Y-m-d H:m:s'),
-           'updated_at' => date('Y-m-d H:m:s') ));
-        }
-        */
+    { 
+           
+    	 DB::table('medicamentos')->insert([
+            'nombre' => 'Desparasitante para lombrices para perros y gatos',
+            'presentacion' => 'Capsula',
+            'compuesto' => 'Penicilina',
+            'fecha_caducidad' => '2019-12-24',
+            'ml_mg' => '20',
+            'precio' => '100',
+            'laboratorio' =>'CPMax',
+            'urlImagen' => '/storage/Medicamentos/desparasitante.jpg',
+            'paq_unidad' => '2',
+            'controlado' => 'Si',
+            'stock' => '200',
+            'clinica_id' => 1
+        ]);
+
+             DB::table('medicamentos')->insert([
+            'nombre' => 'Tratamiento para Sarna Perros y Gatos',
+            'presentacion' => 'Aerosol',
+            'compuesto' => 'Penicilina',
+            'fecha_caducidad' => '2019-2-2',
+            'ml_mg' => '200',
+            'precio' => '100',
+            'laboratorio' =>'BAYER',
+            'urlImagen' => '/storage/Medicamentos/spraySarna.jpg',
+            'paq_unidad' => '6',
+            'controlado' => 'No',
+            'stock' => '200',
+            'clinica_id' => 1
+        ]);
+
+
+             DB::table('medicamentos')->insert([
+            'nombre' => 'Vacuna para la rabia',
+            'presentacion' => 'Inyectable',
+            'compuesto' => 'Penicilina',
+            'fecha_caducidad' => '2019-11-11',
+            'ml_mg' => '200',
+            'precio' => '100',
+            'laboratorio' =>'FORT DODGE',
+            'urlImagen' => '/storage/Medicamentos/vacunaRabia.png',
+            'paq_unidad' => '6',
+            'controlado' => 'No',
+            'stock' => '200',
+            'clinica_id' => 1
+        ]);
     }
 }
 
