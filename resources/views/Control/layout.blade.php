c232cffe30db191fecb588c26d829aaa30c746a9
Tiffany Cortez <tiffcortez95@gmail.com>
2019-11-23 16:49:50 -0600

    T: Problemas con dropdown del menu

--- resources/views/Control/layout.blade.php
+++ resources/views/Control/layout.blade.php
@@ -96,14 +96,21 @@
                         <a class="nav-link menuControl" href="/control/registro_vacunas"></i>
                             Registro de vacunas</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link menuControl" href="/control/compras_ventas"></i>
-                            Compras y ventas</a>
+                    <li class="nav-item active dropdown">
+                        <a class="dropdown-toggle menuControl" href="#" id="navbardrop" data-toggle="dropdown">
+                            Inventario
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/control/registrar_compra">Registrar Compra</a>
+                            <a class="dropdown-item" href="/control/registrar_inventario">Registrar llegada de artículos</a>
+                        </div>
+                        
                     </li>
                     
                     <li class="nav-item active">
                         <a class="nav-link menuControl" href="/control/historial_mascota"></i>
                             Historial de mascota</a>
+
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link menuControl" href="/control/historial_personal"></i>
@@ -138,8 +145,8 @@
         
     </nav>
 
-          
         @yield('contenido')
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
