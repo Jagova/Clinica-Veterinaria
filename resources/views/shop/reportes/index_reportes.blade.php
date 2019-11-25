c55e1ca938d101b71541b30f06d817d4f756b203
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2019-11-25 16:59:30 -0600

    Se agrego que solo imprima los reportes que siguen desaparecidos
    Se puso una condicion para que solo muestre los reportes que se encuentran en condicion de Abierto.

--- resources/views/shop/reportes/index_reportes.blade.php
+++ resources/views/shop/reportes/index_reportes.blade.php
@@ -5,22 +5,26 @@
 <!--Empiezan tarjetas de reportes-->
   <div class="row m-5" style="height:60vh">
   @foreach ($reportesPerdidos as $reportes)
+
+  @if( $reportes->estado =="Abierto")
+
     <div class="col-3 m-0 p-0 h-100 mh-100">
       <div class="card w-100 h-100" style="width: 18rem;">
-        <div style="height:40vh">
+        <h5 class="card-title" style="">{{$reportes->titulo}}</h5>
+        <div style="height:40%">
           <img class="card-img-top mh-100" src="{{ asset($reportes->urlImagen) }}" alt="mascota">
         </div>
-        <div class="card-body" style=" 20hv;">
-          <h5 class="card-title">{{$reportes->titulo}}</h5>
 
-          <p class="card-text">{{$reportes->fecha}}</p>
+        <div class="card-body" style="">
+
+          
+          <strong><p class="card-text">{{$reportes->fecha}}</p></strong>
           <p class="card-text">{{$reportes->descripcion}}</p>
-
-          <p class="card-text">{{$reportes->estado}}</p>
 
         </div>
       </div>
     </div>
+    @endif
   @endforeach
   </div>
 
